75fe439e92df31ccefdafd945ee87e8f30eaa52c
Richard Lea <chigix@zoho.com>
2019-04-30 15:06:29 +0900

    feat(components): basic elements for first milestone

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# RENgular <img src="https://github.com/chigix/rengular/blob/master/src/assets/logo-150.png" align="right" />
+# RenGULAR <img src="https://github.com/chigix/rengular/blob/master/src/assets/logo-150.png" align="right" />
 
 The modern visual novel-style game engine leveraging the power of
 [web](https://www.webcomponents.org/) and [Angular](https://angular.io/) in ultimate stories
 -- open source and free to render interactive storytelling on browsers acrossing
 variety of smart devices upon the modern Web Components standards embracing the
 [New Generation of Internet and Web](https://ieeexplore.ieee.org/document/4620089/?arnumber=4620089).
+
+## Design Overview
+
+![rengular-design](./src/assets/simulation.png)
+
+## TODO
+
+* Schematics for generate a RenGULAR game boilerplate.
 
 ## Development server
 
@@ -29,3 +37,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## License
+
+MIT.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatToolbarModule,
+  MatButtonModule,
+} from '@angular/material';
+import { SimulationModule } from './modules/simulation/simulation.module';
+import { SceneModule } from './modules/scene/scene.module';
+import { SimpleEntryModule } from './modules/simple-entry/simple-entry.module';
+import { SimpleNaviModule } from './modules/simple-navi/simple-navi.module';
+import { TextboxModule } from './modules/textbox/textbox.module';
+import { OarsPocketModule } from './modules/oars-pocket/oars-pocket.module';
+import { ChoiceMenuModule } from './modules/choice-menu/choice-menu.module';
 
 @NgModule({
   declarations: [
@@ -11,10 +24,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    // Routing
     AppRoutingModule,
-    BrowserAnimationsModule
+    // Material
+    MatToolbarModule,
+    MatButtonModule,
+    // RenGULAR Components
+    SimulationModule,
+    SceneModule,
+    SimpleEntryModule,
+    SimpleNaviModule,
+    TextboxModule,
+    OarsPocketModule,
+    ChoiceMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(router: Router) {
+    if (isDevMode()) {
+      console.log('Routes:', JSON.stringify(
+        router.config, (key, value) => value.name || value, 2));
+    }
+  }
+
+}
