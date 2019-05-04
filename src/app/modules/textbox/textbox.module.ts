c786b71841fd6677808f3e21cf75610689a6e6ea
Richard Lea <chigix@zoho.com>
2019-05-04 10:30:49 +0900

    feat(textbox): basic paragraph representation
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/app/modules/textbox/textbox.module.ts
+++ src/app/modules/textbox/textbox.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TextboxComponent } from './textbox.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TextboxComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TextboxComponent],
 })
 export class TextboxModule { }
