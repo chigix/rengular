1492477ca7db631d96fbbe6ad1e6b9cb2a629657
Richard Lea <chigix@zoho.com>
2019-05-22 00:27:23 +0900

    fix: clean imports in scene component

--- src/app/modules/scene/index.ts
+++ src/app/modules/scene/index.ts
@@ -1,4 +1,2 @@
 export { SceneComponent } from './scene.component';
-export { BottomTextSceneComponent } from './bottom-text-scene/bottom-text-scene.component';
-export { NovelTextSceneComponent } from './novel-text-scene/novel-text-scene.component';
 export * from './scene.module';
--- src/app/modules/scene/scene.module.ts
+++ src/app/modules/scene/scene.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SceneComponent } from './scene.component';
-import { BottomTextSceneComponent } from './bottom-text-scene/bottom-text-scene.component';
-import { NovelTextSceneComponent } from './novel-text-scene/novel-text-scene.component';
 import { SceneHostDirective } from './scene-host.directive';
 
 @NgModule({
   declarations: [
     SceneHostDirective,
     SceneComponent,
-    BottomTextSceneComponent,
-    NovelTextSceneComponent,
   ],
   imports: [
     CommonModule,
